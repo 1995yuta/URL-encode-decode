1d7347d4190495d989f8075e7d45c5dd4b19e47b
1995yuta <githubkondoyuta@gmail.com>
2022-11-13 14:43:26 +0900

    説明の追加 例の追加

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function(){
     $('.inputArea').on('click', '.urlBtn', function(){
         // あいうえお
-        // %2582%25A0%2582%25A2%2582%25A4%2582%25A6%2582%25A8
+        
         var oldUrl = document.getElementById('oldUrl');
         var oldUrlText = oldUrl.value;
         // UTF-8コードのタイプで escape エンコードされた escapedString を元の文字列にデコードした値を返します。
